3e158d757165f922047d45ad6ee2941e781a7b77
寒&菡 <31273931+tinglovehan@users.noreply.github.com>
2021-10-16 10:39:23 +0800

    Delete public directory

--- public/javascripts/baidu-statistics.js
+++
@@ -1,8 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?e682bcfed09771669e6bdd5cf891523e";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
-
